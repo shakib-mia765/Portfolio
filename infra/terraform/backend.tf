4f1d0d09d0841c679d31670bb57dfe7bc6579ab6
Shakib Mia <karinebrean8@gmail.com>
2026-07-23 19:54:02 +0600

    Update backend.tf

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,15 +1,10 @@
 terraform {
   backend "s3" {
     bucket = "ultrafaang-terraform-state"
-
     key = "envs/production/terraform.tfstate"
-
     region = "us-east-1"
-
     encrypt = true
-
     dynamodb_table = "ultrafaang-terraform-locks"
-
     kms_key_id = "alias/terraform-state-kms"
   }
 }
